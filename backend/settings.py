6dbb3e8336bf37c973958ff8dbb9eacab9234181
Devicharan <vallurudevicharan@gmail.com>
2026-01-30 13:03:00 +0530

    Added CORS credentials setting

--- backend/settings.py
+++ backend/settings.py
@@ -88,8 +88,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # ================= CORS =================
-CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "https://student-frontend.onrender.com",
+]
+CORS_ALLOW_CREDENTIALS = True
 # ================= DRF & JWT =================
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
